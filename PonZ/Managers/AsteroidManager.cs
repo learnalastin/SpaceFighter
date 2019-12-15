16e4f9ae414a1f475a26185a9054b6b5007f339c
Alex <alexlastin3@gmail.com>
2019-12-15 16:21:06 +0300

    Space invader game , first commit

---
+++ PonZ/Managers/AsteroidManager.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PonZ.Sprites;
+
+namespace PonZ.Managers
+{
+    public class AsteroidManager
+    {
+        private float _timer;
+
+        private List<Texture2D> _textures;
+
+        public bool CanAdd { get; set; }
+
+        public Bullet Bullet { get; set; }
+
+        public int MaxAsteroids { get; set; }
+
+        public float SpawnTimer { get; set; }
+
+        public AsteroidManager(ContentManager content)
+        {
+            _textures = new List<Texture2D>()
+      {
+        content.Load<Texture2D>("Obstacles/Asteroid"),
+      };
+
+            MaxAsteroids = 5;
+            SpawnTimer = 3.5f;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            _timer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            CanAdd = false;
+
+            if (_timer > SpawnTimer)
+            {
+                CanAdd = true;
+
+                _timer = 0;
+            }
+        }
+
+        public Asteroid GetAsteroid()
+        {
+            var texture = _textures[Game1.Random.Next(0, _textures.Count)];
+
+            return new Asteroid(texture)
+            {
+                Colour = Color.Gray,
+                Health = 5,
+                Layer = 0.3f,
+                Position = new Vector2(Game1.ScreenWidth + texture.Width, Game1.Random.Next(0, Game1.ScreenHeight)),
+                Speed = 1 + (float)Game1.Random.NextDouble(),
+            };
+        }
+    }
+}
---
+++ PonZ/Sprites/Asteroid.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace PonZ.Sprites
+{
+    public class Asteroid : Ship
+    {
+        private float _timer;
+
+        public Asteroid(Texture2D texture)
+          : base(texture)
+        {
+            Speed = 0.5f;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            _timer += (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            Position += new Vector2(-Speed, 0);
+
+            // if the enemy is off the left side of the screen
+            if (Position.X < -_texture.Width)
+                IsRemoved = true;
+        }
+
+        public override void OnCollide(Sprite sprite)
+        {
+            // If we crash into a player that is still alive
+            if (sprite is Player && !((Player)sprite).IsDead)
+            {
+                ((Player)sprite).Score.Value++;
+
+                // We want to remove the ship completely
+                IsRemoved = true;
+            }
+
+            // If we hit a bullet that belongs to a player      
+            if (sprite is Bullet && ((Bullet)sprite).Parent is Player)
+            {
+                Health--;
+
+                if (Health <= 0)
+                {
+                    IsRemoved = true;
+                    ((Player)sprite.Parent).Score.Value++;
+                }
+            }
+        }
+    }
+}
